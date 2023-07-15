8374a00769dec83d150f25368c2fbe576ba14018
Zachary Himes <zacharymhimes@gmail.com>
2023-07-14 20:38:00 -0400

    14. Arrow Function Basics

---
+++ 14.ArrowFunctionsBasics/Task1/task1.js
@@ -0,0 +1,30 @@
+// Replace Function Expressions with arrow 
+// functions in the code below: 
+
+function ask(question, yes, no) 
+{
+    if (confirm(question)) yes();
+    else no();
+}
+
+ask
+(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+);
+
+// Answer:
+
+let ask = (question, yes, no) => 
+    {
+        if(confirm(question)) yes();
+        else no();
+    }
+
+ask 
+(
+    "Do you agree?",
+    function() { alert("You agreed."); },
+    function() { alert("You canceled the execution."); }
+);
